40642ebd0d799483d6bfdef4ca32256600d05cfd
gherardo varando <gherardo.varando@gmail.com>
2021-10-13 16:10:23 +0200

    upd exp

--- run_experiment_toy.sh
+++ run_experiment_toy.sh
@@ -3,5 +3,5 @@
 
 gammas=(0 0.01 0.1 0.5 1 1.5 2 5 10 20 100)
 for gamma in ${gammas[@]}; do
-python main.py -d toy -m vae -g ${gamma} --maxlag 5 --max_epochs 50 --limit_train_batches 10
+python main.py -d toy -m vaedeep -g ${gamma} --maxlag 5 --max_epochs 50 --limit_train_batches 10
 done
