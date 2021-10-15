7522df93ba790f84cce57c4dc0c8d690637ff13b
gherardo varando <gherardo.varando@gmail.com>
2021-10-15 15:18:08 +0200

    flake8 checks and upd logger

--- main.py
+++ main.py
@@ -1,134 +1,126 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# BSD 3-Clause License (see LICENSE file)
+# Copyright (c) Image and Signaling Process Group (ISP) IPL-UV 2021
+# All rights reserved.
+
 """
-LatentGranger Main
-
-
-Anonymized code submitted alongide 
-the manuscript titled 
-Learning Granger Causal Feature Representations 
-
-please do not distribute
+Main script to execute the Latent Granger autoencoder
 """
 
 import os
 import git
-import numpy as np
-import argparse, yaml
+import argparse
+import yaml
 from datetime import datetime
-from shutil import copyfile
 
-import torch
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 # Model
-import models 
+import models
 import loaders
 
 
 def main(args):
 
     # Load YAML config files  into a dict variable
-    
-    
+
     with open(f'configs/models/{args.model}.yaml') as file:
         # The FullLoader parameter handles the conversion from YAML
         # scalar values to Python dictionary format
         model_config = yaml.load(file, Loader=yaml.FullLoader)
-    
+
     with open(f'configs/loaders/{args.loader}.yaml') as file:
         # The FullLoader parameter handles the conversion from YAML
         # scalar values to Python dictionary format
         loader_config = yaml.load(file, Loader=yaml.FullLoader)
-    
-    
+
     with open(f'configs/data/{args.data}.yaml') as file:
         # The FullLoader parameter handles the conversion from YAML
         # scalar values to Python dictionary format
         data_config = yaml.load(file, Loader=yaml.FullLoader)
-    
-    
+
     # Experiment ID
     repo = git.Repo(search_parent_directories=True)
     git_commit_sha = repo.head.object.hexsha[:7]
-    
-    experiment_id = str(datetime.now()) 
-    log_dir =  os.path.join(args.dir, 'logs', args.data, args.model, git_commit_sha, experiment_id) 
-    checkpoints_dir =  os.path.join(args.dir, 'checkpoints', args.data, args.model,
-                                    git_commit_sha, experiment_id) 
-    
+
+    experiment_id = str(datetime.now())
+    log_dir = os.path.join(args.dir, 'logs', args.data, args.model,
+                           git_commit_sha, experiment_id)
+    checkpoints_dir = os.path.join(args.dir, 'checkpoints',
+                                   args.data, args.model,
+                                   git_commit_sha, experiment_id)
+
     # Build model
-    
+
     if model_config['processing_mode'] == 'flat':
-       input_size = data_config['flat_input_size']
+        input_size = data_config['flat_input_size']
     else:
-       input_size = tuple(data_config['input_size'])
-    
-    model_class = getattr(models, model_config['class']) 
-    model = model_class(model_config, input_size, data_config['tpb'],  args.maxlag, args.gamma)
+        input_size = tuple(data_config['input_size'])
+
+    model_class = getattr(models, model_config['class'])
+    model = model_class(model_config, input_size, data_config['tpb'],
+                        args.maxlag, args.gamma)
     print(model)
-    
+
     # Build data models
-    datamodule_class = getattr(loaders, loader_config['class']) 
-    datamodule = datamodule_class(loader_config, data_config, model_config['processing_mode'])
-        
+    datamodule_class = getattr(loaders, loader_config['class'])
+    datamodule = datamodule_class(loader_config, data_config,
+                                  model_config['processing_mode'])
+
     # Loggers
-    # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
-    tb_logger = pl_loggers.TensorBoardLogger(log_dir , name = args.model, version = experiment_id)
-    
+    tb_logger = pl_loggers.TensorBoardLogger(log_dir, name=args.model,
+                                             version=experiment_id)
+
     # Callbacks
     # Init ModelCheckpoint callback, monitoring 'val_loss'
-    checkpoint_callback = ModelCheckpoint(dirpath= checkpoints_dir,
+    checkpoint_callback = ModelCheckpoint(dirpath=checkpoints_dir,
                                           filename='{epoch}-{val_loss:.5f}',
                                           mode='min', monitor='val_loss',
                                           save_last=True, save_top_k=5)
-    
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.0, patience=20, 
+
+    early_stopping = EarlyStopping(monitor='val_loss',
+                                   min_delta=0.0, patience=20,
                                    verbose=False, mode='min', strict=True)
-    
+
     callbacks = [checkpoint_callback]
-    
     if args.earlystop:
-       callbacks += [early_stopping]
-    
-    
-    trainer = pl.Trainer.from_argparse_args(args, logger = [tb_logger], callbacks=callbacks)
-    #trainer = pl.Trainer(accumulate_grad_batches=1, callbacks=callbacks, 
-    #                     gpus=args.gpus, auto_select_gpus= args.gpus > 0,
-    #                     log_every_n_steps=10, logger=[tb_logger], 
-    #                     max_epochs=args.epochs,
-    #                     num_sanity_val_steps=2,
-    #                     weights_summary='full') 
-                
+        callbacks += [early_stopping]
+
+    trainer = pl.Trainer.from_argparse_args(args, logger=[tb_logger],
+                                            callbacks=callbacks)
+
     # Training
     trainer.fit(model, datamodule)
-    
+
     # Test
     trainer.test(ckpt_path='best')
 
 
-
 if __name__ == '__main__':
-
-   # ArgParse
-   parser = argparse.ArgumentParser(description="ArgParse")
-   parser = pl.Trainer.add_argparse_args(parser)
-   parser.add_argument('-m', '--model', default='vae', type=str,
-                     help='name of the model associated to a config file in configs/models/')
-   parser.add_argument('-d', '--data', default='toy', type=str,
-                     help='database name (default: toy) associated to a config file in configs/data/')
-   parser.add_argument('--loader', default='base', type=str,
-                     help='loaders name (default: base) associated to a config file in configs/loaders/')
-   parser.add_argument('--maxlag', default=1, type=int,
-                     help='maxlag (default: 1)')
-   parser.add_argument('-g', '--gamma', default=0, type=float,
-                     help='gamma regulazier for granger penalty (default: 0)')
-   parser.add_argument('--earlystop', action = 'store_true', help = 'whether to use early stopping')
-   parser.add_argument('--dir', default="experiment", type=str, help = 'experiemnt directory')
-   
-   
-   
-   args = parser.parse_args()
-   main(args)
+    # ArgParse
+    parser = argparse.ArgumentParser(description="ArgParse")
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser.add_argument('-m', '--model', default='vae', type=str,
+                        help='name of the model associated to a config file ' +
+                             'in configs/models/')
+    parser.add_argument('-d', '--data', default='toy', type=str,
+                        help='database name (default: toy) associated to a ' +
+                             'config file in configs/data/')
+    parser.add_argument('--loader', default='base', type=str,
+                        help='loaders name (default: base) associated ' +
+                             'to a config file in configs/loaders/')
+    parser.add_argument('--maxlag', default=1, type=int,
+                        help='maxlag (default: 1)')
+    parser.add_argument('-g', '--gamma', default=0, type=float,
+                        help='gamma regulazier for granger' +
+                              'penalty (default: 0)')
+    parser.add_argument('--earlystop', action='store_true',
+                        help='whether to use early stopping')
+    parser.add_argument('--dir', default="experiment",
+                        type=str, help='experiemnt directory')
+    args = parser.parse_args()
+    main(args)
--- models/bvae.py
+++ models/bvae.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+# BSD 3-Clause License (see LICENSE file)
+# Copyright (c) Image and Signaling Process Group (ISP) IPL-UV 2021,
+# All rights reserved.
+
 """
 LatentGranger Model Class
-
-BSD 3-Clause License (see LICENSE file)
-
-Copyright (c) Image and Signaling Process Group (ISP) IPL-UV 2021, 
-All rights reserved.
 """
-
 import numpy as np
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from .loss import *
+from .loss import granger_loss
+
 
 class bvae(pl.LightningModule):
-    def __init__(self, config, input_size, tpb, maxlag = 1, gamma = 0.0):
+    def __init__(self, config, input_size, tpb, maxlag=1, gamma=0.0):
         super().__init__()
-        
+
         # Save hyperparameters
         self.save_hyperparameters()
-        
+
         # Config
         self.config = config
-        self.tpb = tpb 
-         
+        self.tpb = tpb
+
         # coefficient for granger regularization
         self.gamma = float(gamma)
 
         # coefficient for beta-VAE
         self.beta = float(config['beta'])
 
-        # lag 
+        # lag
         self.lag = int(maxlag)
 
         # read from config
-        self.latent_dim = config['latent_dim']  
+        self.latent_dim = config['latent_dim']
         self.encoder_out = config['encoder']['out_features']
         self.decoder_out = config['decoder']['out_features']
 
-        ### define distribution for VAE
-        self.N = torch.distributions.Normal(0,1) 
+        # define distribution for VAE
+        self.N = torch.distributions.Normal(0, 1)
 
         self.input_size = input_size
 
@@ -50,186 +51,142 @@ class bvae(pl.LightningModule):
         # Encoder
         self.encoder_layers = nn.ModuleList()
 
-        in_ = self.input_size 
+        in_ = self.input_size
         for out_ in self.encoder_out:
             self.encoder_layers.append(nn.Linear(in_, out_))
             in_ = out_
-        
-        self.mu_layer = nn.Linear(in_, self.latent_dim) 
-        self.sigma_layer = nn.Linear(in_, self.latent_dim) 
+
+        self.mu_layer = nn.Linear(in_, self.latent_dim)
+        self.sigma_layer = nn.Linear(in_, self.latent_dim)
 
         in_ = self.latent_dim
         # Decoder
         self.decoder_layers = nn.ModuleList()
         for out_ in self.decoder_out:
-            self.decoder_layers.append(nn.Linear(in_, out_))                                     
+            self.decoder_layers.append(nn.Linear(in_, out_))
             in_ = out_
-                                       
+
         # Output
         self.output_layer = nn.Linear(in_, self.input_size)
 
         self.drop_layer = nn.Dropout(p=0.4)
 
-        
     def forward(self, x):
         # Define forward pass
-    
+
         # Reshape to (b_s*tpb, ...)
         x_shape_or = np.shape(x)[2:]
         x = torch.reshape(x, (self.tpb,) + x_shape_or)
-        
+
         # Encoder
         for i in np.arange(len(self.encoder_out)):
             x = self.encoder_layers[i](x)
             x = F.leaky_relu(x, 0.01)
-            
-        mu = self.mu_layer(x)   
-        sigma = torch.exp(self.sigma_layer(x)) 
 
+        mu = self.mu_layer(x)
+        sigma = torch.exp(self.sigma_layer(x))
 
-        x = mu + sigma * self.N.sample(mu.shape) 
-        
+        x = mu + sigma * self.N.sample(mu.shape)
+
         # Latent representation
         # Reshape to (b_s, tpb, latent_dim)
-        x_latent = torch.reshape(x, (1, self.tpb,-1))
+        x_latent = torch.reshape(x, (1, self.tpb, -1))
 
         # Decoder
         for i in np.arange(len(self.decoder_out)):
             x = self.decoder_layers[i](x)
             x = F.leaky_relu(x, 0.01)
-                    
+
         # Output
         x = self.output_layer(x)
-        
+
         # Reshape to (b_s, tpb, ...)
         x = torch.reshape(x, (1, self.tpb,) + x_shape_or)
-        
+
         return x, x_latent, mu, sigma
-    
+
     def training_step(self, batch, idx):
         x, target = batch
 
         # Define training step
         x_out, x_latent, mu, sigma = self(x)
-        
+
         # Compute loss
         loss = torch.tensor([0.0])
-        
+
         kl_loss = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).mean()
-        mse_loss = F.mse_loss(x_out,x, reduction = 'mean')
+        mse_loss = F.mse_loss(x_out, x, reduction='mean')
         loss += mse_loss + self.beta * kl_loss
-        
+
         # Granger loss
-        Granger_loss =  granger_loss(x_latent, target, maxlag = self.lag)
-        loss += self.gamma * Granger_loss
+        g_loss = granger_loss(x_latent, target, maxlag=self.lag)
+        loss += self.gamma * g_loss
 
+        self.log('loss', {"train": loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('mse_loss', {"train": mse_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('kl_loss', {"train": kl_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('granger_loss', {"train": g_loss}, on_step=False,
+                 on_epoch=True, logger=True)
 
-        return {'loss': loss,
-                'mse_loss': mse_loss.detach().item(),
-                'kl_loss' : kl_loss.detach().item(),
-                'granger_loss': Granger_loss.detach().item()}
- 
-    def training_epoch_end(self, training_step_outputs):
-        # Loggers
-        loss = [batch['loss'].detach().cpu().numpy() for batch in training_step_outputs]
-        self.logger.experiment[0].add_scalars("losses", {"train": np.nanmean(loss)}, global_step=self.current_epoch)
-        self.log('train_loss', np.nanmean(loss), on_step=False, on_epoch=True, prog_bar=True, logger=False)
+        return loss
 
-        mse_loss = np.array([batch['mse_loss'] for batch in training_step_outputs])
-        self.logger.experiment[0].add_scalars("mse_losses", {"train": np.nanmean(mse_loss)}, global_step=self.current_epoch)
-
-        kl_loss = np.array([batch['kl_loss'] for batch in training_step_outputs])
-        self.logger.experiment[0].add_scalars("kl_losses", {"train": np.nanmean(kl_loss)}, global_step=self.current_epoch)
-
-        Granger_loss = np.array([batch['granger_loss'] for batch in training_step_outputs])
-        self.logger.experiment[0].add_scalars("granger_losses", {"train": np.nanmean(Granger_loss)}, global_step=self.current_epoch)
-
-
-        
-    def validation_step(self, batch, idx): 
+    def validation_step(self, batch, idx):
         x, target = batch
-        
         # Define training step
         x_out, x_latent, mu, sigma = self(x)
-        
         # Compute loss
         loss = torch.tensor([0.0])
 
-
-        kl_loss = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).mean()
-        mse_loss = F.mse_loss(x_out,x, reduction = 'mean')
+        kl_loss = (sigma ** 2 + mu ** 2 - torch.log(sigma) - 1 / 2).mean()
+        mse_loss = F.mse_loss(x_out, x, reduction='mean')
         loss += mse_loss + self.beta * kl_loss
-        
+
         # Granger loss
-        Granger_loss =  granger_loss(x_latent, target, maxlag = self.lag)
-        loss += self.gamma * Granger_loss
+        g_loss = granger_loss(x_latent, target, maxlag=self.lag)
+        loss += self.gamma * g_loss
 
-        return {'val_loss': loss,
-                'val_mse_loss': mse_loss.detach().item(),
-                'val_kl_loss': kl_loss.detach().item(),
-                'val_granger_loss': Granger_loss.detach().item()}
-
-    def validation_epoch_end(self, validation_step_outputs):
-        # Loggers
-        loss = [batch['val_loss'].detach().cpu().numpy() for batch in validation_step_outputs]
-        self.logger.experiment[0].add_scalars("losses", {"val": np.nanmean(loss)}, global_step=self.current_epoch)
-        self.log('val_loss', np.nanmean(loss), on_step=False, on_epoch=True, prog_bar=True, logger=False)
-
-        mse_loss = np.array([batch['val_mse_loss'] for batch in validation_step_outputs])
-        self.logger.experiment[0].add_scalars("mse_losses", {"val": np.nanmean(mse_loss)}, global_step=self.current_epoch)
-
-        kl_loss = np.array([batch['val_kl_loss'] for batch in validation_step_outputs])
-        self.logger.experiment[0].add_scalars("kl_losses", {"val": np.nanmean(kl_loss)}, global_step=self.current_epoch)
-
-
-        Granger_loss = np.array([batch['val_granger_loss'] for batch in validation_step_outputs])
-        self.logger.experiment[0].add_scalars("granger_losses", {"val": np.nanmean(Granger_loss)}, global_step=self.current_epoch)
-
+        self.log('loss', {"val": loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('mse_loss', {"val": mse_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('kl_loss', {"val": kl_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('granger_loss', {"val": g_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('val_loss', loss)
 
     def test_step(self, batch, idx):
         x, target = batch
-        
         # Define training step
         x_out, x_latent, mu, sigma = self(x)
-        
         # Compute loss
         loss = torch.tensor([0.0])
-        
 
-        kl_loss = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).mean()
-        mse_loss = F.mse_loss(x_out,x, reduction = 'mean')
-        loss += mse_loss + self.beta * kl_loss       
+        kl_loss = (sigma ** 2 + mu ** 2 - torch.log(sigma) - 1/2).mean()
+        mse_loss = F.mse_loss(x_out, x, reduction='mean')
+        loss += mse_loss + self.beta * kl_loss
 
-        Granger_loss =  granger_loss(x_latent, target, maxlag = self.lag)
-        loss += self.gamma * Granger_loss
-        
+        g_loss = granger_loss(x_latent, target, maxlag=self.lag)
+        loss += self.gamma * g_loss
 
-        # Loggers
-        return {'test_loss': loss,
-                'test_mse_loss': mse_loss.detach().item(),
-                'test_kl_loss': kl_loss.detach().item(),
-                'test_granger_loss': Granger_loss.detach().item()}
- 
-    def test_epoch_end(self, test_step_outputs):
-        # Loggers
-        loss = [batch['test_loss'].detach().cpu().numpy() for batch in test_step_outputs]
-        self.logger.experiment[0].add_scalars("losses", {"test": np.nanmean(loss)}, global_step=self.current_epoch)
-        self.log('test_loss', np.nanmean(loss), on_step=False, on_epoch=True, prog_bar=True, logger=False)
-
-        mse_loss = np.array([batch['test_mse_loss'] for batch in test_step_outputs])
-        self.logger.experiment[0].add_scalars("mse_losses", {"test": np.nanmean(mse_loss)}, global_step=self.current_epoch)
-
-        kl_loss = np.array([batch['test_kl_loss'] for batch in test_step_outputs])
-        self.logger.experiment[0].add_scalars("kl_losses", {"test": np.nanmean(kl_loss)}, global_step=self.current_epoch)
-
-
-        Granger_loss = np.array([batch['test_granger_loss'] for batch in test_step_outputs])
-        self.logger.experiment[0].add_scalars("granger_losses", {"test": np.nanmean(Granger_loss)}, global_step=self.current_epoch)
-
+        self.log('loss', {"test": loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('mse_loss', {"test": mse_loss},
+                 on_step=False, on_epoch=True, logger=True)
+        self.log('kl_loss', {"test": kl_loss}, on_step=False,
+                 on_epoch=True, logger=True)
+        self.log('granger_loss', {"test": g_loss}, on_step=False,
+                 on_epoch=True, logger=True)
 
     def configure_optimizers(self):
+        # read parameters
+        lr = self.config['optimizer']['lr']
+        weight_decay = self.config['optimizer']['weight_decay']
         # build optimizer
         trainable_params = filter(lambda p: p.requires_grad, self.parameters())
-        optimizer = torch.optim.Adam(trainable_params, lr=self.config['optimizer']['lr'], weight_decay=self.config['optimizer']['weight_decay'])
-       
+        optimizer = torch.optim.Adam(trainable_params,
+                                     lr=lr, weight_decay=weight_decay)
         return optimizer
