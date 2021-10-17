e00998c2def7f48098c654c75ee94ad4701eea8f
gherardo varando <gherardo.varando@gmail.com>
2021-10-17 21:55:27 +0200

    fix run experiment script

--- run_experiment_toy.sh
+++ run_experiment_toy.sh
@@ -4,5 +4,5 @@ arch=$1
 
 gammas=(0 0.01 0.1 0.5 1 1.5 2 5 10 20 100)
 for gamma in ${gammas[@]}; do
-python main.py -m ${arch} -d toy -g ${gamma} --maxlag 5 --max_epochs 50 --limit_train_batches 20 --earlystop
+python main.py --arch ${arch} -d toy -g ${gamma} --maxlag 5 --max_epochs 100 --limit_train_batches 20 --earlystop
 done
