1468acff2c518a828e5522f9b48c68b914f46c51
gherardo varando <gherardo.varando@gmail.com>
2021-12-01 13:16:33 +0100

    dir change

--- run_toy.sh
+++ run_toy.sh
@@ -3,5 +3,5 @@
 #gammas=(0 10 100 250 500 750 1000 2500 5000)
 gammas=(120 140 160 180 200 220 240)
 for gamma in ${gammas[@]}; do
-python main.py --arch vaetoy -d toy -g ${gamma} --maxlag 5 --max_epochs 300 --limit_train_batches 10
+python main.py --arch vaetoy -d toy -g ${gamma} --maxlag 5 --max_epochs 300 --limit_train_batches 10 --dir experiments_toy
 done
