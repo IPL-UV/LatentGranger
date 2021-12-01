950224c328b0bb0c62ebe85b221f2a49d7ab0dc3
gherardo varando <gherardo.varando@gmail.com>
2021-12-01 10:33:17 +0100

    gammas

--- run_africa_ndvi.sh
+++ run_africa_ndvi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gammas=(0 5000 10000 20000 21000 22000 23000 24000 25000 30000 35000 40000 50000 60000 70000)
+gammas=(40000 50000 60000 70000 80000 90000 100000)
 for gamma in ${gammas[@]}; do
 python main.py --arch vae -d africandvi -g ${gamma} --maxlag 10 --max_epochs 200 --limit_train_batches 5
 done
