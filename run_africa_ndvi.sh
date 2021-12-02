70243f4b3e8397a9ae312475b772718a9c63c979
gherardo varando <gherardo.varando@gmail.com>
2021-12-02 08:55:35 +0100

    replicates

--- run_africa_ndvi.sh
+++ run_africa_ndvi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gammas=(40000 50000 60000 70000 80000 90000 100000)
+gammas=(100000 10000 10000 10000)
 for gamma in ${gammas[@]}; do
 python main.py --arch vae -d africandvi -g ${gamma} --maxlag 10 --max_epochs 200 --limit_train_batches 5
 done
