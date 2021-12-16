d73ebeaf444e01f7b3dd681e275d9cf5b09570ae
gherardo varando <gherardo.varando@gmail.com>
2021-12-16 10:05:37 +0100

    upd explain file

--- explain.py
+++ explain.py
@@ -264,10 +264,10 @@ if args.latint:
     for j in range(mu.shape[1]):
         latent_int_max = mu.clone() 
         #latent_int_max[:,j] = latent_max[j]
-        latent_int_max[:,j] += 3 * sigma[:,j]
+        latent_int_max[:,j] += 1 * sigma[:,j]
         latent_int_min = mu.clone() 
         #latent_int_min[:,j] = latent_min[j]
-        latent_int_min[:,j] -= 3 * sigma[:,j]
+        latent_int_min[:,j] -= 1 * sigma[:,j]
         out_int_max = model.decoder(latent_int_max)
         out_int_min = model.decoder(latent_int_min)
         diff_int_max = (out_int_max - x_out[0,:,:]) #/ (x_out[0,:,:] + 0.0001)
